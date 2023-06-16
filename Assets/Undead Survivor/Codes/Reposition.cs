14ad86b4252ae6774feae68fc487619de549a356
exceptionError-jin <kj98224849@gamil.com>
2023-06-16 16:32:39 +0900

    무한 맵 생성

---
+++ Assets/Undead Survivor/Codes/Reposition.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+            return;
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        
+
+
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffX = Mathf.Abs(dirX);
+        float diffY = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if(diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+        }
+    }
+
+}
